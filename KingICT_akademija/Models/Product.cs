c8b5fc0c090e13abe830c1da603cd53619f3b1da
mkljaic20 <mkljaic20@foi.hr>
2024-06-21 15:14:21 +0200

    Added Product model class.

---
+++ KingICT_akademija/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace KingICT_akademija.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string ShortDescription { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+    }
+}
