5f4242a177fc050c9f7c88bdf955e1c0907588c2
mkljaic20 <mkljaic20@foi.hr>
2024-06-21 16:15:02 +0200

    Added code to Program.cs to register HttpClient for ProductService. Also added code to ProductController that gets a response from API and fetches all products.

--- KingICT_akademija/Controllers/ProductController.cs
+++ KingICT_akademija/Controllers/ProductController.cs
@@ -1,6 +1,27 @@
-﻿namespace KingICT_akademija.Controllers
+﻿using KingICT_akademija.Models;
+using KingICT_akademija.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KingICT_akademija.Controllers
 {
-    public class ProductController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
     {
+        private readonly ProductService productService;
+
+        public ProductController(ProductService _productService)
+        {
+            productService = _productService;
+        }
+
+        //GET api/products
+        //retrieves all products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await productService.GetProductsAPI();
+            return Ok(products);
+        }
     }
 }
